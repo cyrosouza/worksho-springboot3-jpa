d0956f51d2046dd9ceccb029c714db1fb7c128d3
Cyro Souza <cyro_souza_89@hotmail.com>
2024-09-19 15:08:51 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/projetospringjpa/projetoSpringJPA/entities/User.java
+++ src/main/java/com/projetospringjpa/projetoSpringJPA/entities/User.java
@@ -1,11 +1,14 @@
 package com.projetospringjpa.projetoSpringJPA.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -97,6 +103,10 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
